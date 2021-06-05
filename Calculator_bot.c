82c9485fd13baca48fd0e84e93c6a8d2cc47c405
Zunnoon Tahir <78951403+Zunnoont@users.noreply.github.com>
2021-06-05 16:44:36 +1000

    Minor edits

--- Calculator_bot.c
+++ Calculator_bot.c
@@ -1,38 +1,40 @@
+// Calculator Bot
+// Calculator_bot.c
+// 
+// This program was written by Zunnoon Tahir
+// This program acts as a virtual calculator
+// Providing various mathematical 
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
-
 #define MAX_ARRAY_SIZE 1000
 
-void basic_operations(double int1, double int2){
+void basic_operations(double int1, double int2) {
     char operator;
-    // make it so the user can simply enter any 
-    // mathematical basic operation
-    // such as 1 + 12
     printf("Enter operator: Addition: +, Subtraction: -, Division: /, Multiplication: *: ");
-    
     scanf(" %c", &operator);
     if(operator == '+') {
         printf("Enter numbers to be added: ");
         scanf("%lf %lf", &int1, &int2);
-        printf("%lf", int1+int2);
+        printf("%lf", int1 + int2);
     }
     else if(operator == '-') {
         printf("Enter numbers to be subtracted: ");
         scanf("%lf %lf", &int1, &int2);
-        printf("%lf", int1-int2);
+        printf("%lf", int1 - int2);
     }
     else if(operator == '/') {
         printf("Enter numbers to be divided: ");
         scanf("%lf %lf", &int1, &int2);
-        printf("%lf", int1/int2);
+        printf("%lf", int1 / int2);
     }
     else if(operator == '*') {
         printf("Enter numbers to be multiplied: ");
         scanf("%lf %lf", &int1, &int2);
-        printf("%lf", int1*int2); 
+        printf("%lf", int1 * int2); 
     }
 }
 void powers(double int1, double int2) {
@@ -43,47 +45,45 @@ void powers(double int1, double int2) {
     char string3[MAX_ARRAY_SIZE] = "Pi";
     printf("Enter base: Integer, e, Pi:\n");
     scanf("%s", &string);
-    if(strcmp(string,string1) == 0){
+    if(strcmp(string, string1) == 0) {
         printf("Enter Numbers. E.g: entering 2 3 is 2^3: ");
         scanf("%lf %lf", &int1, &int2);
         result = pow(int1,int2);
         printf("%lf", result);
     }
-    else if(strcmp(string,string2) == 0){
+    else if(strcmp(string, string2) == 0) {
         printf("Enter power of base e (e^(integer): ");
         scanf("%lf", &int1);
         result = pow(M_E, int1);
         printf("%lf", result);
     }
-    else if(strcmp(string,string3) == 0){
+    else if(strcmp(string, string3) == 0) {
         printf("Enter power of base Pi (Pi^(Integer): ");
         scanf("%lf", &int1);
         result = pow(M_PI, int1);
         printf("%lf", result);
     }
-    else{
+    else {
         printf("Invalid base\n");
     }
-
 }
-void root(){
+void root() {
     double degree;
     double root;
     double base;
     double result;
     printf("Enter degree of root (Entering 2 is equivelant to a square root): ");
     scanf("%lf", &degree);
-    root = 1/degree;
+    root = 1 / degree;
     printf("Enter number inside root: ");
     scanf("%lf", &base);
-    if(base >= 0){
+    if(base >= 0) {
         result = pow(base, root);
         printf("%lf", result);
     }
-    else{
+    else {
         printf("Number inside root must be positive or zero");
     }
-    
 }
 void trigonometric() {
     char trigstring[MAX_ARRAY_SIZE];
@@ -95,25 +95,21 @@ void trigonometric() {
     scanf("%s", &trigstring);
     printf("Enter angle: ");
     scanf("%lf", &angle);
-    radians = 0.0174532925*angle;
-    if(strcmp(trigstring, "Sin") == 0 || strcmp(trigstring, "sin") == 0){
+    radians = 0.0174532925 * angle;
+    if(strcmp(trigstring, "Sin") == 0 || strcmp(trigstring, "sin") == 0) {
         result = sin(radians);
         printf("%lf", result);
     }
-    else if(strcmp(trigstring, "Cos") == 0 || strcmp(trigstring, "cos") == 0){
+    else if(strcmp(trigstring, "Cos") == 0 || strcmp(trigstring, "cos") == 0) {
         result = cos(radians);
         printf("%lf", result);
-
     }
-    else if(strcmp(trigstring, "Tan") == 0 || strcmp(trigstring, "tan") == 0){
+    else if(strcmp(trigstring, "Tan") == 0 || strcmp(trigstring, "tan") == 0) {
         result = tan(radians);
         printf("%lf", result);
     }
-    
-
-
 }
-void inverse_trig(){
+void inverse_trig() {
     char inverse_trig[MAX_ARRAY_SIZE];
     double value;
     double result;
@@ -123,57 +119,50 @@ void inverse_trig(){
     scanf("%s", &inverse_trig);
     printf("Enter value inside inverse function from between -1 and 1, e.g: entering 1 is arcsin(1): ");
     scanf("%lf", &value);
-    if(value < -1 || value > 1){
-        while(value < -1 || value > 1){
+    if(value < -1 || value > 1) {
+        while(value < -1 || value > 1) {
             printf("Invalid value, enter value from -1 to 1: ");
             scanf("%lf", &value);
-
         }
     }
-    if(strcmp(inverse_trig, "Arcsin") == 0 || strcmp(inverse_trig, "arcsin") == 0){
+    if(strcmp(inverse_trig, "Arcsin") == 0 || strcmp(inverse_trig, "arcsin") == 0) {
         result = asin(value);
         angle = 180/M_PI * result;
         printf("%lf", angle);
-        
     }
-    else if(strcmp(inverse_trig, "Arccos") == 0 || strcmp(inverse_trig, "arccos") == 0){
+    else if(strcmp(inverse_trig, "Arccos") == 0 || strcmp(inverse_trig, "arccos") == 0) {
         result = acos(value);
         angle = 180/M_PI * result;
         printf("%lf", angle);
-
     }
-    else if(strcmp(inverse_trig, "Arctan") == 0 || strcmp(inverse_trig, "arctan") == 0){
+    else if(strcmp(inverse_trig, "Arctan") == 0 || strcmp(inverse_trig, "arctan") == 0) {
         result = atan(value);
         angle = 180/M_PI * result;
         printf("%lf", angle);
     }
-
 }
-void factorials(){
+void factorials() {
     int number;
     int increment;
     int factorial = 1;
     printf("Enter factorial: ");
     scanf("%d", &number);
-    if(number < 0){
+    if(number < 0) {
         number = number * -1;
-        for(increment = 1; increment <= number; increment++){
+        for(increment = 1; increment <= number; increment++) {
             factorial = factorial * increment;
         }
         factorial = factorial * -1;
         printf("%d", factorial);
     }
-    else{
-        for(increment = 1; increment <= number; increment++){
+    else {
+        for(increment = 1; increment <= number; increment++) {
             factorial = factorial * increment;
         } 
         printf("%d", factorial);
     }
-    
-   
 }
-int main(void) {
-
+void corefunction(void){
     char operator;
     double int1;
     double int2;
@@ -187,33 +176,29 @@ int main(void) {
     printf("Enter 5 for roots\n");
     printf("Enter 6 for factorials\n");
     scanf("%d", &options);
-    if(options == 1){
+    if(options == 1) {
         basic_operations(int1, int2);
     }
-    else if(options== 2){
+    else if(options == 2) {
         powers(int1, int2);
     }
-    else if(options == 3){
+    else if(options == 3) {
         trigonometric();
     }
-    else if(options == 4){
+    else if(options == 4) {
         inverse_trig();
-        
     }
-    else if(options == 5){
+    else if(options == 5) {
         root();
     }
-    else if(options == 6){
+    else if(options == 6) {
         factorials();
     }
     else{
         printf("Invalid operation choice");
     }
-    
-    
-
-    
+}
+int main(void) {
+    corefunction();
     return 0;
-
-    
 }
